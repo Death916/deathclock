aa76d7f7601504232e9a8af16bb8f759daa27be5
Death916 <tavn1992@gmail.com>
2021-08-21 04:03:33 -0700

    init

---
+++ deathclock/main.py
@@ -0,0 +1,19 @@
+#deathclock
+import time
+
+def main():
+    
+    class clock():
+        
+        def alarm():
+            return
+
+
+
+    class weather():
+        pass
+
+    class gui():
+        pass
+
+
