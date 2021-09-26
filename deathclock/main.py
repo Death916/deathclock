e9c49c50c99d0be68d12e2a7bc0ccd6a3e8b7cec
TrentN <Tavn1992@gmail.com>
2021-09-26 03:40:45 -0700

    added time nd semi calendar

--- deathclock/main.py
+++ deathclock/main.py
@@ -6,7 +6,8 @@ import dearpygui.dearpygui as dpg
 class clock():
 
     def time_and_date(self):
-        print(time.asctime())
+        return time.asctime()
+
 
     def alarm(self):
         alarm_time = None
@@ -25,18 +26,29 @@ class weather():
 
 class gui():
     def show_time(self):
-
+        clock1 = clock()
+        timenow = clock1.time_and_date()
         with dpg.window(label="time"):
-            dpg.add_text(time.asctime())
-            dpg.start_dearpygui()
+            dpg.add_text(timenow)
+
+
+    def show_cur_weather(self):
+        with dpg.window(label="Current Weather"):
+            dpg.load_image()
+
+    def alarm_button(self):
+        with dpg.window(label="Set Alarm"):
+            dpg.add_date_picker(track_offset=.5, indent=6)
 
 
 def main():
     gui1 = gui()
     gui1.show_time()
+    gui1.alarm_button()
+    dpg.start_dearpygui()
 
 
-    
+
 if __name__ == "__main__":
     main()
 
