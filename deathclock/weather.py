92ed012c4d0792d85bfdf3c9979111e10235d8a9
Death916 <mail@trentnelson.dev>
2025-03-23 07:28:27 -0700

    trying to fit on pi screen

--- deathclock/weather.py
+++ deathclock/weather.py
@@ -38,7 +38,7 @@ class Weather:
             curl_command = [
                 "curl",
                 "-s",  # Silent mode
-                "v2.wttr.in/Sacramento.png?0pq&scale=2",  # Fetch weather data for Sacramento
+                "v2.wttr.in/Sacramento.png?0pq&scale=.5",  # Fetch weather data for Sacramento
                 "-o",
                 screenshot_path,
             ]
